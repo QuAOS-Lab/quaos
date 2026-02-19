cd2b1d7a4e68e2d6777f8f56c339d7c59123dbed
Luca Dellantonio <l.dellantonio@exeter.ac.uk>
2026-02-19 17:42:07 +0000

    cleaned up import and `--all--`, hope I did not mess up here

--- src/sympleq/core/paulis/__init__.py
+++ src/sympleq/core/paulis/__init__.py
@@ -2,8 +2,7 @@ from .pauli import Pauli
 from .pauli_string import PauliString
 from .pauli_sum import PauliSum
 from .pauli_object import PauliObject
-from .utils import (check_mappable_via_clifford, are_subsets_equal, commutation_graph)
+from .utils import (check_mappable_via_clifford, are_subsets_equal)
 
 __all__ = ['PauliString', 'PauliSum', 'Pauli', 'PauliObject',
-           'check_mappable_via_clifford', 'are_subsets_equal',
-           'commutation_graph']
+           'check_mappable_via_clifford', 'are_subsets_equal']
