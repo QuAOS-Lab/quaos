2fbd908740cc2449ce2db291d1ccf02fc671bacf
Luca Dellantonio <l.dellantonio@exeter.ac.uk>
2026-02-24 11:46:53 +0000

    fixed import

--- src/sympleq/core/paulis/__init__.py
+++ src/sympleq/core/paulis/__init__.py
@@ -2,7 +2,7 @@ from .pauli import Pauli
 from .pauli_string import PauliString
 from .pauli_sum import PauliSum
 from .pauli_object import PauliObject
-from .utils import (check_mappable_via_clifford, are_subsets_equal)
+from .utils import check_mappable_via_clifford, mod_inv, hamiltonian_mean, covariance_matrix  # , are_subsets_equal
 
 __all__ = ['PauliString', 'PauliSum', 'Pauli', 'PauliObject',
-           'check_mappable_via_clifford', 'are_subsets_equal']
+           'check_mappable_via_clifford', 'mod_inv', 'hamiltonian_mean', 'covariance_matrix']  # , 'are_subsets_equal']
